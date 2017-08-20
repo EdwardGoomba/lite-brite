57f62baae21f6fd35c72fbd59d7642c2b53efd22
EdwardGoomba <ask@edwardvdesign.com>
2017-08-20 13:16:57 -0400

    Added jquery to select and animate lights

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 function main() {
 
+  var colorClass = '';
+
+  $('.select-color').on('click', function() {
+    var selectedColor = $(this).attr('class');
+
+    switch (selectedColor) {
+      case 'select-color cyan not-selected':
+        colorClass = 'cyan';
+        break;
+      case 'select-color yellow not-selected':
+        colorClass = 'yellow';
+        break;
+      case 'select-color magenta not-selected':
+        colorClass = 'magenta';
+        break;
+                         }
+
+    $(this).removeClass('not-selected');
+    $(this).siblings().addClass('not-selected');
+
+  });
+
+  $('.box').on('click', function() {
+    $(this).toggleClass(colorClass);
+
+  });
+
+  $('.toggle-blink').on('click', function() {
+    if (colorClass) {
+      $('.toggle-blink').toggleClass('opacity');
+      setInterval(function() {
+        $('.box.magenta, .box.yellow, .box.cyan').toggleClass('blink');
+
+      }, 500);
+    }
+
+  });
 }
 
 $(document).ready(main);
